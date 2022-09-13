2c8740ebd863876271949d2dc4c63543d77999ab
Guilherme-FCM <guilherme_fcm@hotmail.com>
2022-09-13 19:59:39 -0300

    Finish mobile app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import AppLoading from "expo-app-loading"
+import { AppLoading } from 'expo';
+import { StatusBar } from 'react-native';
 
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
-import Home from "./src/pages/Home"
+
+import Routes from './src/routes';
 
 export default function App() {
-  const [ fontsLoaded ] = useFonts({
+  const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_500Medium,
     Ubuntu_700Bold
-  })
+  });
 
-  if (!fontsLoaded){ return <AppLoading/> }
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
 
   return (
     <>
-      <StatusBar style="auto" backgroundColor="transparent" translucent/>
-      <Home />
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Routes />
     </>
   );
 }
